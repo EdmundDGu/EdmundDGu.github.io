27af61dfe78824a2e62e75aeb30143b5b5051cb6
EdmundDGu <1204272271@qq.com>
2020-10-16 16:00:40 +0800

    github main

--- 连续时间实指数信号.py
+++ 连续时间实指数信号.py
@@ -7,12 +7,14 @@ import matplotlib.pyplot as plt
 t = np.linspace(-1.0,1.0,1000)  #通过linspace函数指定t的取值范围
 plt.ylim(0,5)                          #定义纵轴取值范围
 plt.subplot(221)     #显示的位置
-plt.title(u'exp(-t)')   #图名
+plt.title(u'exp(t)')   #图名
 ft = np.exp(-t)              #调用exp函数计算指数信号
 plt.plot(t,ft)                #绘图
 plt.subplot(221)
-plt.title(u'exp(t)')
 ft1 = np.exp(t)
 plt.plot(t,ft1)
+plt.subplot(221)
+ft2 = np.exp(0*t)
+plt.plot(t,ft2)
 plt.show()
 
